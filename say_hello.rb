7cf66c4dc1e27255e2eb44ee1efca977bc8810f4
Alexis Priore <alexispriore@gmail.com>
2019-12-12 16:15:12 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,7 @@
 # Build your say_hello method here
+
+say_hello( "Gabriela" )
+
+def greeting(name="Ruby programmer", language="Ruby")
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
